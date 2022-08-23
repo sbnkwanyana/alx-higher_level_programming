d08934734699505d8953cbe708a9ecff279b4d9a
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-08-23 23:47:22 +0200

    print last digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number is not int or number is not float:
-        return
-    if number < 0:
-        number = number * -1
-    value = number % 10
-    print(value, end="")
-    return (value)
+    tmp = int(repr(number)[-1])
+    print("{}".format(tmp), end="")
+    return tmp
