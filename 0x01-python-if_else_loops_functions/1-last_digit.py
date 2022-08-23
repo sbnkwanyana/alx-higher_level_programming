e13746ee6c7b76a2681b5418e508be007a582544
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-08-23 23:08:30 +0200

    solution from class mate as i count figure out a way to correctly obtain the negative value

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number < 0:
-    number = number * -1
-if number % 10 > 5:
-    print(f"Last digit of {number:d} is {number % 10} and is greater than 5")
-elif number % 10 == 0:
-    print(f"Last digit of {number:d} is {number % 10} and is 0")
-elif number % 10 < 6:
-    print(f"Last digit of {number:d} is {number % 10}", end="")
-    print(f" and is less than 6 and not 0")
+ldigit = number % 10 if number > 0 else int(repr(number)[-1]) * -1
+if (ldigit > 5):
+    print("Last digit of {:d} is {:d} and is greater than 5"
+          .format(number, ldigit))
+elif (ldigit == 0):
+    print("Last digit of {:d} is {:d} and is 0"
+          .format(number, ldigit))
+else:
+    print("Last digit of {:d} is {:d} and is less than 6 and not 0"
+          .format(number, ldigit))
