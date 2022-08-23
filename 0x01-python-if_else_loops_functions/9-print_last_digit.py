ca03b84bb71e734cc7e29cad69d6130ab3e3f365
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-08-23 14:29:18 +0200

    return if number is not an int or float

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
+    if number is not int or number is not float:
+        return
     if number < 0:
         number = number * -1
     value = number % 10
