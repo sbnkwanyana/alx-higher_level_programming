e8993d1fcd914f78a15ed498ea740c7c7eb14c7e
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-08-25 11:40:17 +0200

    function prints the number of and the list of its arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+if len(sys.argv) == 1:
+    print("{} arguments.".format(0))
+elif len(sys.argv) == 2:
+    print("{} argument:".format(1))
+    print("{}: {}".format(1, sys.argv[1]))
+else:
+    i = 1
+    print("{} arguments:".format(len(sys.argv) - 1))
+    while i < len(sys.argv):
+        print("{}: {}".format(i, sys.argv[i]))
+        i += 1
