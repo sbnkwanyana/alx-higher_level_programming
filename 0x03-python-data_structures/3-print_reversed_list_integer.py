e960a8aeaeeb25cfb5a5c7dc3b73c30a881ce8d1
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-08-29 09:38:42 +0200

    removed testing code, fixed reverse issue and made it more concise to read

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    reverse = my_list.sort(reverse=True)
-    for i in my_list:
+    for i in my_list.sort(reverse=True):
         print("{:d}".format(i))
-
-print_reversed_list_integer([1, 2, 3, 4])
