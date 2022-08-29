9d101beac606d965838a1eeb17d94cd423077c8a
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-08-29 11:05:37 +0200

    updated to check for empty strings

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     multi_return = ()
-    if sentence is None:
+    if sentence is None or sentence == "":
         multi_return = (0, None)
     else:
         multi_return = (len(sentence), sentence[0])
