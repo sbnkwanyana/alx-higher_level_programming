960007db95a97fdb4bf0b45dd144e98b1cb25183
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-08-29 09:35:26 +0200

    updated formating to use the :d flag

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,4 +2,6 @@
 def print_reversed_list_integer(my_list=[]):
     reverse = my_list.sort(reverse=True)
     for i in my_list:
-        print("{}".format(i))
+        print("{:d}".format(i))
+
+print_reversed_list_integer([1, 2, 3, 4])
