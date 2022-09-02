d05d5aad3d823e62305a9bdd5e2dc52c952435d6
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-09-02 14:10:31 +0200

    fixed checking condition for empty dictionary to equals zero instead of less than zero

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,10 +2,14 @@
 def best_score(a_dictionary):
     best = 0
     best_key = ""
-    if a_dictionary is None or len(a_dictionary) < 0:
+    if a_dictionary is None or len(a_dictionary) == 0:
         return (None)
     for key in a_dictionary:
         if a_dictionary[key] > best:
             best = a_dictionary[key]
             best_key = key
     return best_key
+
+my_dict = {}
+best_key = best_score(my_dict)
+print("Best score: {}".format(best_key))
