36322b3305de1292b168fa898e193770e51ea229
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-09-02 21:10:48 +0200

    altered code to return a modified dictionary instead of a new one

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    modified_dictionary = a_dictionary.copy()
+    key_list = []
     for key, and_value in a_dictionary.items():
         if value == and_value:
-            del modified_dictionary[key]
-    return modified_dictionary
+            key_list.append(key)
+    for i in key_list:
+        del a_dictionary[i]
+    return a_dictionary
