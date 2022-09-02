a896363b03b4c6dafdc937f9442d2c55bafb2c27
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-09-02 11:15:24 +0200

    Removed test code. fixed pycodestyle line length

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    unique_list = []
+    unique = []
     if my_list is None or len(my_list) < 0:
         return my_list
-    [unique_list.append(number) for number in my_list if number not in unique_list]
-    return (sum(unique_list))
-
-my_list = [1, 2, 3, 1, 4, 2, 5]
-result = uniq_add(my_list)
-print("Result: {:d}".format(result))
+    [unique.append(number) for number in my_list if number not in unique]
+    return (sum(unique))
