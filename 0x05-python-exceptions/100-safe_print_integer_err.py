9566889f8f1e02599f42d36fedee465686d93ae7
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-09-12 15:13:46 +0200

    refactored code to utilize except else

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,7 +5,8 @@ import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-        return True
     except ValueError as valerr:
         print("Exception: {}".format(valerr), file=sys.stderr)
         return False
+    else:
+        return True
