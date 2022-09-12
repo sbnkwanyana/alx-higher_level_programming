de37e4adba2d0df7a496544a133eda042c311d5d
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-09-12 15:19:23 +0200

    used generic exception instead of specific exception to catch broader range of exceptions

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,8 +5,8 @@ import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-    except ValueError as valerr:
-        print("Exception: {}".format(valerr), file=sys.stderr)
+    except Exception as ex:
+        print("Exception: {}".format(ex), file=sys.stderr)
         return False
     else:
         return True
