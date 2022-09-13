5ff05e2d593fc5927e6644db7958856d936c7387
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-09-14 01:24:14 +0200

    added documentation to module, class and initialization function

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""This module contains the square class"""
 class Square:
+    """Class represents a square"""
     def __init__(self, size):
+        """ initializes private instance attribute with provided size """
         self._size = size
