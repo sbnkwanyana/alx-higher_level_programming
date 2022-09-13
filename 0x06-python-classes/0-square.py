97a847d58f29aba465f17efc7fd59002842ff4a3
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-09-14 01:42:26 +0200

    added coding: utf-8 clause

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 """ This module contains the square class"""
 class Square:
     """This is an empty class of type square"""
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 """This module contains the square class"""
 class Square:
     """Class represents a square"""
