cb9a166d4bf7df9d86c6993f9b01286dca50983e
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-09-14 01:44:52 +0200

    removed clause. converted to multiline documentation

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-""" This module contains the square class"""
+"""
+This module contains the square class
+"""
 class Square:
-    """This is an empty class of type square"""
+    """
+    This is an empty class of type square
+    """
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-"""This module contains the square class"""
+"""
+This module contains the square class
+"""
 class Square:
-    """Class represents a square"""
+    """
+    Class represents a square
+    """
     def __init__(self, size):
-        """ initializes private instance attribute with provided size """
+        """
+        initializes private instance attribute with provided size
+        """
         self.__size = size
