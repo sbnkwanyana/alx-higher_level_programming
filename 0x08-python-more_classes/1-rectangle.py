32ace987e936bbd198909c203551c416f52c63c0
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-09-19 23:41:19 +0200

    edited width setter property to set width instead of height

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -43,7 +43,7 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self.__height = value
+        self.__width = value
 
     @height.setter
     def height(self, value):
