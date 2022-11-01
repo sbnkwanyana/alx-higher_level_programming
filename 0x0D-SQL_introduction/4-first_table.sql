6f0ccec984affcbeb9c615e7109ba527d37089d7
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-11-01 14:27:54 +0200

    int to INT

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 -- creates a table if it does not exist, with properties id and name
- CREATE TABLE IF NOT EXISTS first_table (id int, name VARCHAR(256));
+ CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
