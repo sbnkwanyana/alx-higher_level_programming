2c5992fb474a4e96dab9ff734d17fa119038d70c
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-11-02 14:27:07 +0200

    9. Cities by States

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- lists all cities and states by city id
-SELECT cities.id, cities.name, states.id FROM cities INNER JOIN states ON cities.state_id = states.id ORDER BY cities,id;
+SELECT cities.id, cities.name, states.id FROM cities INNER JOIN states ON cities.state_id = states.id ORDER BY cities.id;
