f567d45ecf5f5159144799d4db8509df6146ab35
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-11-29 16:07:09 +0200

    9. Add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,17 +3,15 @@
 const process = require('process');
 const args = process.argv;
 
-function add(a, b)
-{
-    console.log(a + b);
+function add (a, b) {
+  console.log(a + b);
 }
 
 const a = Number(args[2]);
 const b = Number(args[3]);
 
 if (!a || !b) {
-    console.log(NaN);
-}
-else {
-    add(a, b);
+  console.log(NaN);
+} else {
+  add(a, b);
 }
