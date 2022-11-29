c7e990b1b362de4067a277946e7ff05f60379283
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-11-29 15:55:38 +0200

    8. Square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const process = require('process');
+const args = process.argv;
+
+const size = Number(args[2]);
+if (!size) {
+    console.log('Missing size');
+}
+else {
+    for (let i = 0; i < size; i++)
+    {
+        for (let j = 0; j < size; j++)
+        {
+            process.stdout.write(`X`);
+        }
+        j = 0;
+        console.log();
+    }
+}
