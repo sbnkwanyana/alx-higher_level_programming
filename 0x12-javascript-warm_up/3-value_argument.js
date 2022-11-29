de8a425c5de65cb926b31600b86192af40d27980
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-11-29 12:51:30 +0200

    3. Value of my argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -3,12 +3,9 @@
 const process = require('process');
 const args = process.argv;
 
-let arg = args[2];
-if (!arg)
-{
-    console.log('No argument');
-}
-else
-{
-    console.log(arg);
+const arg = args[2];
+if (!arg) {
+  console.log('No argument');
+} else {
+  console.log(arg);
 }
