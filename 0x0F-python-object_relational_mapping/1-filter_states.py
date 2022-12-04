bc4386caf8d19ff2a501ecb85c63c0cbc0708ef1
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-12-04 14:24:44 +0200

    changed to passing string into execute from passing a string variable

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,10 +11,9 @@ if __name__ == "__main__":
     user = sys.argv[1]
     passwd = sys.argv[2]
     database = sys.argv[3]
-    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
     db = MySQLdb.connect(host=host, user=user, passwd=passwd, db=database)
     cursor = db.cursor()
-    cursor.execute(query)
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     data = cursor.fetchall()
     for row in data:
         print(row)
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,10 +9,9 @@ if __name__ == "__main__":
     user = sys.argv[1]
     passwd = sys.argv[2]
     database = sys.argv[3]
-    query = f"SELECT * FROM states WHERE name='{sys.argv[4]}' ORDER BY id ASC"
     db = MySQLdb.connect(host=host, user=user, passwd=passwd, db=database)
     cursor = db.cursor()
-    cursor.execute(query)
+    cursor.execute(f"SELECT * FROM states WHERE name='{sys.argv[4]}' ORDER BY id ASC")
     data = cursor.fetchall()
     for row in data:
         print(row)
