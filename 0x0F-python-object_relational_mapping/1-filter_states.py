678e96955322f40b9f597e97a2c5b70949ae83c7
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-12-04 14:46:26 +0200

    connect using utf8

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     user = sys.argv[1]
     passwd = sys.argv[2]
     database = sys.argv[3]
-    db = MySQLdb.connect(host=host, user=user, passwd=passwd, db=database)
+    db = MySQLdb.connect(host=host, user=user, passwd=passwd, db=database, charset="utf8")
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     data = cursor.fetchall()
