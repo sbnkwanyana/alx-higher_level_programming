f31cfeca69628da20cd59cec0e06afc27ebd3a86
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-12-17 01:28:17 +0200

    first line must be bin bash

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 # Bash script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
 curl -sH "Content-Type: application/json" -d "$(cat "$2")" "$1"
