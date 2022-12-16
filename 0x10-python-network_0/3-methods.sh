9c3fb584513ea5158ed6f0242b0c8120752f9074
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-12-16 08:47:17 +0200

    requirment 3 lines long

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/bash
-# Bash script that takes in a URL
-# and displays all HTTP methods the server will accept.
-
+# Bash script that takes in a URL and displays all HTTP methods the server will accept.
 curl -Is "$1" | grep "Allow:" | cut -d " " -f 2-
