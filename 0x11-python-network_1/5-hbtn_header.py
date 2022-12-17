6d10b0d40169410ce133404b90fdb6b0fc72b915
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-12-17 03:28:43 +0200

    5. Response header value #1

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -8,5 +8,5 @@ import sys
 import requests
 
 if __name__ == '__main__':
-    res = requests.get(sys.argv[1])
-    print(res.headers['X-Request-Id'])
+    response = requests.get(sys.argv[1])
+    print(response.headers.get("X-Request-Id"))
