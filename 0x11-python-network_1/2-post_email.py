cac957bdd04c5090c0059a2bee669db2b3229263
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-12-17 03:02:33 +0200

    2. POST an email #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -13,4 +13,4 @@ if __name__ == "__main__":
     data = data.encode("utf-8")
     req = request.Request(sys.argv[1], data)
     with request.urlopen(req) as response:
-        print(response.read.encode("utf-8"))
+        print(response.read().decode("utf-8"))
