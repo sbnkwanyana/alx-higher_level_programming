794e2d3c63b50eb49a3450e402844fc03f76e326
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-12-17 03:22:13 +0200

    4. What's my status? #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,10 +3,9 @@
 Python script that fetches https://alx-intranet.hbtn.io/status
 using the requests package
 """
+import requests
 
 if __name__ == '__main__':
-    import requests
-
     response = requests.get('https://alx-intranet.hbtn.io/status')
     print('Body response:')
     print("\t- type:{}".format(type(response.text)))
