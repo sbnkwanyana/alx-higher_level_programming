48926cd54a6391c43c727ce6ca1800e454078d55
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2022-12-17 04:05:50 +0200

    length comparison between list and int

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,7 +8,7 @@ import requests
 
 if __name__ == '__main__':
     url = "http://0.0.0.0:5000/search_user"
-    if sys.argv >= 2:
+    if len(sys.argv) >= 2:
         value = {"q": sys.argv[1]}
     else:
         value = {"q": ""}
